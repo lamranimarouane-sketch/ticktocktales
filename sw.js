e52f03c83ad6e7a40f7b146e7d90a3916d70b7e7
lamranimarouane-sketch <lamrani.marouane@gmail.com>
2026-02-27 10:38:48 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'ticktock-v1';
+const FILES = [
+  '/ticktocktales/',
+  '/ticktocktales/index.html',
+  '/ticktocktales/manifest.json',
+  '/ticktocktales/icon-192.png',
+  '/ticktocktales/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r =>
+      r || fetch(e.request).catch(() =>
+        caches.match('/ticktocktales/index.html')
+      )
+    )
+  );
+});
